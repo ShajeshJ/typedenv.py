8364d50fdaede4f1ecebf18d3259bb4cec7e024f
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2024-05-15 22:57:23 -0400

    feat: implement function to determine if given type is union type

--- tests/test_annotations.py
+++ tests/test_annotations.py
@@ -26,6 +26,28 @@ def test__allow_annotations_none__true_cases(annotations: typing.Any):
     str | int,
     _dummy_func,
     _DummyClass,
+    dict[str, None],
+    list[str | None],
 ])
 def test__allow_annotations_none__false_cases(annotations: typing.Any):
     assert typedenv.annotations.allows_none_type(annotations) == False
+
+@pytest.mark.parametrize("annotations", [
+    typing.Optional[int],
+    typing.Union[int, list[str], None],
+    None | int,
+])
+def test__is_union_type__true_cases(annotations: typing.Any):
+    assert typedenv.annotations.is_union_type(annotations) == True
+
+@pytest.mark.parametrize("annotations", [
+    str,
+    typing.Callable[[None], None],
+    typing.Annotated[None, int],
+    _dummy_func,
+    _DummyClass,
+    dict[str, str],
+    list[str | int],
+])
+def test__is_union_type__false_cases(annotations: typing.Any):
+    assert typedenv.annotations.is_union_type(annotations) == False
--- typedenv/annotations.py
+++ typedenv/annotations.py
@@ -1,6 +1,11 @@
 import types
 import typing
 
+def is_union_type(t: typing.Any) -> bool:
+    """Check if the given type annotation is a Union type."""
+    generic_type = typing.get_origin(t)
+    return generic_type is typing.Union or generic_type is types.UnionType
+
 def allows_none_type(t: typing.Any) -> bool:
     """Check if the given type annotation allows None as a value."""
     if t is None:
