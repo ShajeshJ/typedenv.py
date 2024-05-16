b3fe4234ce065c0afbd0a130f6d5b2f5fa35cf3a
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2024-05-16 00:15:35 -0400

    feat: replace old function with get_usable_args

--- tests/test_annotations.py
+++ tests/test_annotations.py
@@ -12,32 +12,63 @@ class _DummyClass: ...
 @pytest.mark.parametrize(
     "annotations",
     [
-        typing.Optional[int],
-        typing.Union[int, list[str], None],
-        None | int,
-        None,
+        pytest.param(typing.Optional[int], id="typing.Optional"),
+        pytest.param(typing.Union[None, int, int | None], id="typing.Union"),
+        pytest.param(int | None, id="built-in Union"),
+        pytest.param(dict[int, None], id="built-in dict"),
+        pytest.param(typing.Annotated[None, int], id="typing.Annotated"),
+        pytest.param(tuple[None, int], id="built-in tuple"),
     ],
 )
-def test__allow_annotations_none__true_cases(annotations: typing.Any):
-    assert typedenv.annotations.get_usable_args(annotations) == True
+def test__get_usable_args__int_and_none(annotations: typing.Any):
+    assert typedenv.annotations.get_usable_args(annotations) == (int,)
 
 
 @pytest.mark.parametrize(
     "annotations",
     [
-        str,
-        typing.Callable[[int], None],
-        typing.Annotated[None, int],
-        typing.Union[str, int],
-        str | int,
-        _dummy_func,
-        _DummyClass,
-        dict[str, None],
-        list[str | None],
+        pytest.param(typing.Union[None, None], id="typing.Union"),
+        pytest.param(dict[None, None], id="built-in dict"),
+        pytest.param(tuple[None], id="built-in tuple"),
+        pytest.param(list[None], id="built-in list"),
+        pytest.param(set[None], id="built-in set"),
+        pytest.param(typing.Annotated[None, None], id="typing.Annotated"),
     ],
 )
-def test__allow_annotations_none__false_cases(annotations: typing.Any):
-    assert typedenv.annotations.get_usable_args(annotations) == False
+def test__get_usable_args__only_none(annotations: typing.Any):
+    assert typedenv.annotations.get_usable_args(annotations) == ()
+
+
+@pytest.mark.parametrize(
+    ("annotations", "expected"),
+    [
+        pytest.param(typing.Union[int, str, int | str], (int, str), id="typing.Union"),
+        pytest.param(int | str, (int, str), id="built-in Union"),
+        pytest.param(dict[int, str], (int, str), id="built-in dict"),
+        pytest.param(typing.Annotated[str, int], (str, int), id="typing.Annotated"),
+        pytest.param(tuple[int, str], (int, str), id="built-in tuple"),
+        pytest.param(list[int], (int,), id="built-in list"),
+        pytest.param(set[int], (int,), id="built-in set"),
+        pytest.param(typing.Callable[..., int], (Ellipsis, int), id="typing.Callable"),
+        pytest.param(
+            tuple[str | None, list[None]], (str | None, list[None]), id="Inner None"
+        ),
+    ],
+)
+def test__get_usable_args__no_none(annotations: typing.Any, expected: typing.Any):
+    assert typedenv.annotations.get_usable_args(annotations) == expected
+
+
+@pytest.mark.parametrize(
+    "annotations",
+    [
+        pytest.param(str, id="built-in str"),
+        pytest.param(_dummy_func, id="function"),
+        pytest.param(_DummyClass, id="class"),
+    ],
+)
+def test__get_usable_args__no_args(annotations: typing.Any):
+    assert typedenv.annotations.get_usable_args(annotations) == ()
 
 
 @pytest.mark.parametrize(
--- typedenv/annotations.py
+++ typedenv/annotations.py
@@ -1,20 +1,15 @@
 import types
 import typing
 
+
 def is_union_type(t: typing.Any) -> bool:
     """Check if the given type annotation is a Union type."""
     generic_type = typing.get_origin(t)
     return generic_type is typing.Union or generic_type is types.UnionType
 
-def allows_none_type(t: typing.Any) -> bool:
-    """Check if the given type annotation allows None as a value."""
-    if t is None:
-        return True
 
-    generic_type = typing.get_origin(t)
-    generic_args = typing.get_args(t)
-
-    if generic_type is not typing.Union and generic_type is not types.UnionType:
-        return False
-
-    return types.NoneType in generic_args
+def get_usable_args(t: typing.Any) -> tuple[typing.Any, ...]:
+    """Get all non-None arguments for a type annotation.
+    An empty tuple is returned for type annotations that don't take arguments.
+    """
+    return tuple(arg for arg in typing.get_args(t) if arg not in (None, type(None)))
